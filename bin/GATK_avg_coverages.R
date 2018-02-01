4594252c4aa6793060846183529e534e1aabf721
Stephen Kelly <stevekm@users.noreply.github.com>
2018-02-01 18:29:04 -0500

    update R script sourcing external file

--- bin/GATK_avg_coverages.R
+++ bin/GATK_avg_coverages.R
@@ -6,7 +6,14 @@
 
 
 # ~~~~~ FUNCTIONS ~~~~~ # 
-source("tools.R")
+tryCatch({
+  source("tools.R")
+},
+error=function(cond) {
+  path <- Sys.getenv('PATH')
+  bin_dir <- unlist(strsplit(x = path, split = ':'))[1]
+  source(file.path(bin_dir, "tools.R"))
+})
 
 read.sample_interval_summary.file <- function(file){
     # read the GATK DepthOfCoverage sample_interval_summary file
