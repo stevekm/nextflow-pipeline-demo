0980b31638be4c1469b4cd45428838c2b472a942
Stephen Kelly <stevekm@users.noreply.github.com>
2018-02-21 15:00:26 -0500

    update submit script to use makefile target

--- submit.sh
+++ submit.sh
@@ -3,6 +3,9 @@
 # submit the Nextflow pipeline as a job to run on the SGE HPC
 # NOTE: requires that all nodes can qsub jobs
 
+target="${1:-'xxxxxxxxxxxxxxxxxxx'}"
+[ "$xxxxxxxxxxxxxxxxxxx" == 'xxxxxxxxxxxxxxxxxxx' ] && printf "please specify a target to make" && exit 1 || :
+
 qsub_logdir="$(readlink -f .)/logs"
 mkdir -p "${qsub_logdir}"
 
@@ -10,5 +13,5 @@ job_name="nextflow-exome"
 
 qsub -wd $PWD -o :${qsub_logdir}/ -e :${qsub_logdir}/ -j y -N "$job_name" <<E0F
     set -x
-    make exome
+    make "${target}"
 E0F
